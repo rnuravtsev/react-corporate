edf58af3c9f20b06b738f9681804c9ec869df8a8
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-12-09 00:06:57 +0300

    commit 4: накидал стилей и адаптив

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,18 +5,25 @@ import {questions} from "../../configs/questions";
 
 export const Card: React.FC<ICardProps> = (props) => {
   return (
-    <div>
-      <div>
+    <div className="card">
+      <div className='card_question'>
         {questions[0].question.text}
       </div>
-      <ul>
-        <li>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
+      <ul className="card_answers-list">
+        <li className="answers-list_item">
+          <div className="answer-variant-lighter" />
+          <button className="answers-list_button" type="button">1</button>
+        </li>
+        <li className="answers-list_item">
+          <div className="answer-variant-lighter" />
+          <button className="answers-list_button" type="button">2</button>
+        </li>
+        <li className="answers-list_item">
+          <div className="answer-variant-lighter" />
+          <button className="answers-list_button" type="button">3</button>
         </li>
       </ul>
-      <button type="button">next</button>
+      <button className="card_next-step-button" type="button">next</button>
     </div>
   );
 };
