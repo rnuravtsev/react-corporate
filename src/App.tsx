9a64dccccc06397444b1c64789e2ac02c31764ee
rnuravtsev <muravtsev21@gmail.com>
2021-12-08 21:57:59 +0300

    Добавил normalize.css, убрал из индекса всё, что связано с PWA, прикоснулся к глобальным стилям и добавил изображения для вопросов

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header/Header';
 import './App.css';
+import 'normalize.css/normalize.css';
 import { Footer } from "./components/Footer/Footer";
 import { Main } from "./components/Main/Main";
 
