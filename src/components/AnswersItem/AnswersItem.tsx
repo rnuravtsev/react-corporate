c91260ab1f5f4293dd0628dea0a1d1b2dc4168c1
rnuravtsev <muravtsev21@gmail.com>
2021-12-10 22:05:48 +0300

    Добавил переменные для текста, иконки для ответов, стилизация выбора ответа.

--- src/components/AnswersItem/AnswersItem.tsx
+++ src/components/AnswersItem/AnswersItem.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../images/checkmark.svg';
+import '../../images/cross.svg';
 import classNames from 'classnames';
 
 interface IAnswersItem {
   text: string,
-  isCorrect?: boolean,
+  isCorrect: boolean,
   hint: string,
   percent: number,
 }
 
 
 const AnswersItem: React.FC<IAnswersItem> = ({isCorrect, text, hint, percent})  => {
+  const [selected, setSelected] = useState(false);
+  const onButtonClick = () => {
+    setSelected(true);
+  };
   return (
-    // TODO: Проблемное место, накидал на скорую руку, просто чтобы было
-    <li className={classNames("answers__item",{"": isCorrect === null || undefined}, {"answers__item_correct": isCorrect}, {"answers__item_wrong": !isCorrect})}>
-      <button className="answers__button" type="button">{text}</button>
-      <div className="answers__info">
-        <span className="answers__percentage">{percent}%</span>
-        <span className="answers__hint">{hint}</span>
-      </div>
+    <li className={classNames("answers__item", {"answers__item_correct": selected && isCorrect}, {"answers__item_wrong": selected && !isCorrect})}>
+      {selected && (
+        <span className="answers__percentage">
+          {percent}%
+        </span>
+      )}
+      <button className={"answers__button"} type="button" onClick={onButtonClick}>{text}</button>
+      {selected && (
+        <div className="answers__info">
+          <span className="answers__hint">{hint}</span>
+        </div>
+      )}
     </li>
 
   );
